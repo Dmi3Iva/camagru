bba6b06785f1e76abe154fe766701d2fab43cda9
Dddd <dddd@ddd.com>
2022-09-04 12:23:27 +0300

    Init docker answer simple request

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const app = http.createServer((request, response)=>{
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write('simlpe answer');
-	response.end();
-});
-
-app.listen(3000);
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+// const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const res = require('express/lib/response');
+
+dotenv.config();
+// Mongo DB connection
+// const database = process.env.MONGOLAB_URI;
+// mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
+// .then(() => console.log('e don connect'))
+// .catch(err => console.log('not able to connect', err));
+
+app.get('/', (request, response)=>{
+    response.writeHead(200);
+    response.write('ok, working, root, defently working');
+    response.send();
+});
+
+app.get('/users', (request, response)=>{
+    response.writeHead(200);
+    response.write('ok, working, users');
+    response.send();
+});
+
+app.set('view engine', 'ejs');
+
+//Routes
+// app.use('/', require('./routes/login'));
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log("Server has started at port: " + PORT))
