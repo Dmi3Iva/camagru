4816f472c064ed5297ceb5b5c00d86ca71f3f5b7
Dmitriy Ivanov <dyuryIvanov@sberbank.ru>
2022-09-09 23:54:25 +0300

    fix nodemon in docker not reload correctly

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { registerView, loginView } = require('../controllers/loginController');
+const express = require("express");
+const { registerView, loginView } = require("../controllers/loginController");
 const { dashboardView } = require("../controllers/dashboardController");
 const { protectRoute } = require("../auth/protect");
 const router = express.Router();
 
-router.get('/register', registerView);
-router.get('/login', loginView);
+router.get("/register", registerView);
+router.get("/login", loginView);
 router.get("/dashboard", protectRoute, dashboardView);
 
 module.exports = router;
