b3205b0bd4e0d2c6827fccb2a4546b01d9957c88
Dmitriy Ivanov <dyuryIvanov@sberbank.ru>
2022-09-10 00:13:41 +0300

    add styles for login, changed working port to 3002

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const User = require('./models/User');
-const session = require('express-session');
+const User = require("./models/User");
+const session = require("express-session");
 
 dotenv.config();
 
 const database = process.env.MONGOLAB_URI;
-mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
-.then(() => console.log('connected to DB SUCCESS'))
-.catch(err => console.log('FAILED to connect to DB', err));
+mongoose
+  .connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("connected to DB SUCCESS"))
+  .catch((err) => console.log("FAILED to connect to DB", err));
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
-
-app.use(session({
-    secret:'oneboy',
+app.use(
+  session({
+    secret: "oneboy",
     saveUninitialized: true,
-    resave: true
-  }));
+    resave: true,
+  })
+);
 
 //Routes
-app.use('/', require('./routes/login'));
-app.use(express.urlencoded({extended: false}));
+app.use("/", require("./routes/login"));
+app.use(express.urlencoded({ extended: false }));
 
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, console.log("Server has started at port: " + PORT))
+const PORT = process.env.PORT;
+app.listen(PORT, console.log("Server has started at port: " + PORT));
